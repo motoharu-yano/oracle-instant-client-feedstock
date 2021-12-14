7439651ce959a5b74bd3c847026618e24f75347d
motoharu-yano <muzhedgehog@list.ru>
2021-12-14 13:14:16 +0300

    add oracle-instantclient-sqlplus

---
+++ oracle-instantclient-sqlplus/recipe/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd $PREFIX
+mkdir -p share
+mkdir -p lib
+
+cd share
+#cp -a ${SRC_DIR}/instantclient-sqlplus/. .
+
+cp ${SRC_DIR}/instantclient-sqlplus/libsqlplus.so $PREFIX/lib/
+cp ${SRC_DIR}/instantclient-sqlplus/libsqlplusic.so $PREFIX/lib/
+cp ${SRC_DIR}/instantclient-sqlplus/sqlplus $PREFIX/bin/
