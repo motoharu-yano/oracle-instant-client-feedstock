7665799b7d0d4d8dc55f5dbf1d0f40677d12f927
motoharu-yano <muzhedgehog@list.ru>
2021-12-14 12:28:24 +0300

    adjust instantclient-basic/recipe/build.sh

--- instantclient-basic/recipe/build.sh
+++ instantclient-basic/recipe/build.sh
@@ -7,16 +7,15 @@ mkdir -p lib
 cd share
 cp -a ${SRC_DIR}/instantclient-basic/. .
 
-cd ${SRC_DIR}/instantclient-basic
-cp libclntsh.so.21.1 ../../lib/
-cp libnnz21.so ../../lib/
-cp libociei.so ../../lib/
-cp libocijdbc21.so ../../lib/
-cp liboramysql.so ../../lib/
-cp libocci.so.21.1 ../../lib/
-cp libclntshcore.so.21.1 ../../lib
+cp ${SRC_DIR}/instantclient-basic/libclntsh.so.21.1 $PREFIX/lib/
+cp ${SRC_DIR}/instantclient-basic/libnnz21.so $PREFIX/lib/
+cp ${SRC_DIR}/instantclient-basic/libociei.so $PREFIX/lib/
+cp ${SRC_DIR}/instantclient-basic/libocijdbc21.so $PREFIX/lib/
+cp ${SRC_DIR}/instantclient-basic/liboramysql.so $PREFIX/lib/
+cp ${SRC_DIR}/instantclient-basic/libocci.so.21.1 $PREFIX/lib/
+cp ${SRC_DIR}/instantclient-basic/libclntshcore.so.21.1 $PREFIX/lib
 
-cd ../../lib
+cd $PREFIX/lib
 ln -s libclntsh.so.21.1 libclntsh.so
 ln -s libocci.so.21.1 libocci.so
 ln -s libclntshcore.so.21.1 libclntshcore.so
